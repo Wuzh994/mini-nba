9aa1c6dada99bc4c9456f091dcd046f0aab0b63d
wuzhenghong <2015129064@qq.com>
2022-06-16 00:05:47 +0800

    feat: logos

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { createSSRApp } from 'vue'
 import App from './App.vue'
+import Logos from 'vue-nba-logos'
 
 export function createApp() {
   const app = createSSRApp(App)
+  app.use(Logos)
+
   return {
     app,
   }
