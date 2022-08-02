4b5bcd5309d87fea8927f0c66093d65c1264c3e3
wuzhenghong <2015129064@qq.com>
2022-08-02 16:29:41 +0800

    fix: fetch api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,32 +1,41 @@
-import fetch from 'cross-fetch'
-
 import { API_URL } from '~/constants'
+
+function fetch(url: string): Promise<{statusCode: number, data: any}> {
+  return new Promise((resolve, reject) => {
+    uni.request({
+      url: API_URL.base + url,
+      success: (res) => {
+        resolve(res)
+      },
+      fail: (err) => {
+        reject(err)
+      }
+    })
+  })
+}
 
 class API {
   static async getStandings() {
-    const response = await fetch(`${API_URL.base}v1/current/standings_all.json`)
-    const data = await response.json()
+    const { data, statusCode } = await fetch('/prod/v1/current/standings_all.json')
     return {
       data,
-      status: response.status
+      status: statusCode
     }
   }
 
   static async getGamesByDate(date: string | undefined) {
-    const response = await fetch(`${API_URL.base}v2/${date}/scoreboard.json`)
-    const data = await response.json()
+    const { data, statusCode } = await fetch(`/data/10s/prod/v1/${date}/scoreboard.json`)
     return {
       data,
-      status: response.status
+      status: statusCode
     }
   }
 
-  static async getGameDetails(year: string | undefined, gameId: string | undefined) {
-    const response = await fetch(`${API_URL.details}/data/10s/v2015/json/mobile_teams/nba/${year}/scores/gamedetail/${gameId}_gamedetail.json`)
-    const data = await response.json()
+  static async getGameDetails(date: string | undefined, gameId: string | undefined) {
+    const { data, statusCode } = await fetch(`/data/10s/prod/v1/${date}/${gameId}_boxscore.json`)
     return {
       data,
-      status: response.status
+      status: statusCode
     }
   }
 }
