a3ab318ccff79d09886505cdc6a70330bfce6ffc
wuzhenghong <2015129064@qq.com>
2022-08-02 16:31:48 +0800

    feat: LogoIcon

---
+++ src/components/LogoIcon/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- #ifdef APP-PLUS || H5 -->
+  <component :is="name" size="2em" />
+  <!-- #endif -->
+  <!-- #ifdef MP-WEIXIN -->
+  <IconComp :name="name" size="2em" />
+  <!-- #endif -->
+</template>
+
+<script lang="ts" setup>
+// #ifdef MP-WEIXIN
+import * as Logos from 'vue-nba-logos'
+// #endif
+
+const props = defineProps<{
+  name: string
+}>()
+
+// #ifdef MP-WEIXIN
+const IconComp = (Logos as any)[props.name]
+// #endif
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { createSSRApp } from 'vue'
 import App from './App.vue'
+// #ifdef APP-PLUS || H5
 import Logos from 'vue-nba-logos'
+// #endif
 
 export function createApp() {
   const app = createSSRApp(App)
+  // #ifdef APP-PLUS || H5
   app.use(Logos)
-
+  // #endif
   return {
     app,
   }
